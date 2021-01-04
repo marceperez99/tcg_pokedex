b87546645bff2834ed7a2bfd8915b0ff904b1e9e
Marcelo <marce-1999@hotmail.com>
2021-01-03 21:17:26 -0300

    Updates

---
+++ precache-manifest.7caff60160a61dc21e7b70342af71b62.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff428cd5a8b57deb63fe7220b1f1c099",
+    "url": "/tcg_pokedex/index.html"
+  },
+  {
+    "revision": "2ed7635211384228b69c",
+    "url": "/tcg_pokedex/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "203566e91cad5a3ebc16",
+    "url": "/tcg_pokedex/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2ed7635211384228b69c",
+    "url": "/tcg_pokedex/static/js/2.ce9c52ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tcg_pokedex/static/js/2.ce9c52ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203566e91cad5a3ebc16",
+    "url": "/tcg_pokedex/static/js/main.fefa51d6.chunk.js"
+  },
+  {
+    "revision": "d1343dfbf5bf33c94b8d",
+    "url": "/tcg_pokedex/static/js/runtime-main.d5fb33de.js"
+  },
+  {
+    "revision": "54c4570f8a9927e272272e996d031f58",
+    "url": "/tcg_pokedex/static/media/logo.54c4570f.png"
+  },
+  {
+    "revision": "f396200e401faf429c0675dbc0067a35",
+    "url": "/tcg_pokedex/static/media/notFound.f396200e.png"
+  }
+]);
